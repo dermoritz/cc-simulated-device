21bc6d426f568e6ae4cc3e0e2d2fae77f19e6903
moritz loeser <moritz.loeser@prodyna.com>
2022-08-17 11:02:16 +0200

    add loop

--- src/main/java/com/bosch/ccsimulateddevice/MainLoop.java
+++ src/main/java/com/bosch/ccsimulateddevice/MainLoop.java
@@ -2,13 +2,12 @@ package com.bosch.ccsimulateddevice;
 
 import com.bosch.ccsimulateddevice.device.SimulatedDevice;
 import com.microsoft.azure.sdk.iot.device.DeviceClient;
-import com.microsoft.azure.sdk.iot.device.Message;
-import com.microsoft.azure.sdk.iot.device.MessageSentCallback;
 import com.microsoft.azure.sdk.iot.device.exceptions.IotHubClientException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
@@ -21,17 +20,24 @@ public class MainLoop implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         DeviceClient client = device.getDevice();
-        client.open(false);
-        client.sendEventAsync(new Message("The message"),new MessageSentCallbackImpl(),null);
 
+        log.info("starting report loop...");
+        Mono.fromRunnable(() -> {
+            while (true) {
+                try {
+                    log.info("sending report...");
+                    client.updateReportedProperties(device.getTwin().report(client.getTwin().getReportedProperties()));
+                } catch (InterruptedException | IotHubClientException e) {
+                    throw new IllegalStateException("Problem updating reported properties: ", e);
+                }
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    log.error("Interrupted", e);
+                }
+            }
+        }).subscribe();
     }
 
-    private static class MessageSentCallbackImpl implements MessageSentCallback
-    {
-        @Override
-        public void onMessageSent(Message sentMessage, IotHubClientException exception, Object callbackContext)
-        {
-            log.info("Message sent!");
-        }
-    }
+
 }
